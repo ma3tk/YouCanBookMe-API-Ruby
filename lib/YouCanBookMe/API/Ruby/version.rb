064f8aec74319589be72c34384d8831ed280be5f
ma3tk <masataka0227@gmail.com>
2020-02-07 14:47:36 +0900

    Added first files

---
+++ lib/YouCanBookMe/API/Ruby/version.rb
@@ -0,0 +1,7 @@
+module YouCanBookMe
+  module API
+    module Ruby
+      VERSION = '0.0.1'.freeze
+    end
+  end
+end
---
+++ lib/YouCanBookMe/API/ruby.rb
@@ -0,0 +1,10 @@
+require 'YouCanBookMe/API/Ruby/version'
+
+module YouCanBookMe
+  module API
+    module Ruby
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
